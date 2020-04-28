da2c5bb30a4d100757332024badf480cbfd363eb
mpolitano <marianopolitano@gmail.com>
2020-04-28 13:16:24 -0300

    add sh for run in inv

--- runJPF.sh
+++ runJPF.sh
@@ -7,14 +7,11 @@ repr=("key:concrete" )
 #
 for i in "${repr[@]}"
 do
-	
-		java -version
-		#class=("issta2006.treemap.TreeMap_pred" "Schedule.Schedule" "issta2006.bintree.BinTree_pred" "org.apache.commons.collections4.list.NodeCachingLinkedList")
-	
+		
 		sed -i -e  's/representation=.*/representation='$i'/g' abstractionFunction.propierties
 		
 	
-		#java -Djava.library.path=/home/investigador/mpolitano/jpf/jpf-symbc/lib/ -jar /home/investigador/mpolitano/jpf/jpf-core/build/RunJPF.jar /home/investigador/mpolitano/jpf/jpf-symbc/src/examples/sequences/BankAccountDriverSequences.jpf > $i.txt
-		java -Djava.library.path=Users/mpolitano/development/jpf/jpf-symbc/lib/ -jar /Users/mpolitano/development/jpf/jpf-core/build/RunJPF.jar /Users/mpolitano/development/jpf/jpf-symbc/src/examples/issta2006/treemap/DriverTreeMap.jpf > tmp/$i$j.txt
+		java -Djava.library.path=/home/investigador/mpolitano/jpf/jpf-symbc/lib/ -jar /home/investigador/mpolitano/jpf/jpf-core/build/RunJPF.jar /home/investigador/mpolitano/jpf/jpf-symbc/src/examples/sequences/BankAccountDriverSequences.jpf > $i.txt
+		#java -Djava.library.path=Users/mpolitano/development/jpf/jpf-symbc/lib/ -jar /Users/mpolitano/development/jpf/jpf-core/build/RunJPF.jar /Users/mpolitano/development/jpf/jpf-symbc/src/examples/issta2006/treemap/DriverTreeMap.jpf > tmp/$i$j.txt
 	
 done
